6a1372d97a45731e9cc13ca3e8ee8b4e34988c6c
rogerortiza <rortiz@contractroom.com>
2020-07-31 23:30:44 -0500

    Accessing the courses contents

--- students/urls.py
+++ students/urls.py
@@ -5,4 +5,7 @@ from .views import *
 urlpatterns = [
     path('register/', StudentRegistrationView.as_view(), name='student_registration'),
     path('enroll-course/', StudentEnrollCourseView.as_view(), name='student_enroll_course'),
+    path('courses/', StudentCourseListView.as_view(), name='student_course_list'),
+    path('course/<pk>', StudentCourseDetailView.as_view(), name='student_course_detail'),
+    path('course/<pk>/<module_id>/', StudentCourseDetailView.as_view(), name='student_course_detail_module'),
 ]
